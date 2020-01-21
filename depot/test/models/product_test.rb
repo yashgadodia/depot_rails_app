05551437fada57f2f3e6a20604795bb8c254ccbc
Yash Gadodia <pirsquare.yash@gmail.com>
2020-01-21 13:41:56 +0800

    updated fixtures directive

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -9,6 +9,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products 
   test "product attributes must not be empty" do 
     product = Product.new
     assert product.invalid?
@@ -17,4 +18,42 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                          description: "xyz",
+                          image_url: "xxx.jpeg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than equal to 0.01"],
+        product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+  def new_product(image_url)
+    Product.new( title: "My book title",
+                  description: "wxy",
+                  price: 1,
+                  image_url: image_url)
+  end
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+    http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+      "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+      "#{image_url} shouldn't be valid"
+    end
+  end           
 end
