82018b3d7af75233795ccf1b80d40edbcfdd0a66
Yash Gadodia <pirsquare.yash@gmail.com>
2020-01-22 16:56:39 +0800

    create catalog controller and view for buyer

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
-  def index
+  def index #get the list of products out of db and make it available to the code in the view thatll display the table
+    #we decided to order in alphabetical order
+    @products = Product.order(:title)
   end
 end
--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
 #---
 Rails.application.routes.draw do
+  root 'store#index', as:'store_index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
