18b77d8cf1c06590c3d0dccd754df6addd394b2e
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-29 09:01:22 +0800

    edit view add validation add unit test

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -4,4 +4,12 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    #...
+    #...
+  end
+
 end
