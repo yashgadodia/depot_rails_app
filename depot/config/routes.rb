04445e1ab1cb6bd5c30592fc2d962bc0e4e2c761
Yash Gadodia <pirsquare.yash@gmail.com>
2020-04-29 12:34:29 +0800

    WIP

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :line_items
   root 'store#index', as: 'store_index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_132315) do
+ActiveRecord::Schema.define(version: 2020_04_29_030107) do
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "cart_id"
+    t.string "to"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_04_28_132315) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "line_items", "products"
 end
